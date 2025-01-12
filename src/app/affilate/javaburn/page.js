80df3f17e507907776676ac7f8acc78101bf0a75
indiandeveloper26 <indiandeveloper25@gmail.com>
2025-01-12 08:59:51 -0800

    java burn

--- src/app/affilate/javaburn/page.js
+++ src/app/affilate/javaburn/page.js
@@ -1,11 +1,147 @@
+import Link from 'next/link'
 import React from 'react'
 
 function page() {
   return (
-    <div>
-        <h1>
-            java burn
+    <div className='  bg-white'>
+    
+
+    <div className="  my-1  m-0  w-[100%]   mx-1   flex  justify-center items-center">
+<img className=" w-[90vw]  md:h-[65vh]   sm:w-[100vw] sm:h-[50vh]    " src="/java2.png" alt="My Image" />
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div className=' md:space-x-3    grid md:grid-cols-3 sm:grid-cols-1'>
+
+    <div className=' rounded-md shadow-lg  border-2 border-red-400 overflow-hidden'>
+    <h1 className=' text-center  md:text-[44px]'>
+            2 purchase
         </h1>
+        <br/>
+        <p className=' text-2xl text-center'>
+            60 day supply
+        </p>
+    <div className="  my-1  m-0  w-[100%]   mx-1   flex  justify-center items-center">
+       
+<img className=" w-[100vw]  md:h-[38vh]   sm:w-[100vw] sm:h-[50vh]    " src="/javarm.png" alt="My Image" />
+
+
+</div>
+<div className=' flex justify-center items-center '>
+    <h2 className=' md:text-[55px] text-centerc text-orange-500 '>
+        $79 per pouch
+    </h2>
+</div>
+<div className=' mx-10  animate-bounce  rounded-md    bg-blue-500   flex justify-center items-center'>
+<button className='   border-l md:text-[33px]  text-white  text-center'><Link href={'https://a61434z8ukmedw94gfb5wb-x91.hop.clickbank.net'}>Add to cart</Link> </button>
+</div>
+</div>
+<div className=' rounded-md shadow-lg  border-2 border-red-400 overflow-hidden'>
+    <h1 className=' text-center  md:text-[44px]'>
+            6 purchase
+        </h1>
+        <br/>
+        <p className=' text-2xl text-center'>
+            180 day supply
+        </p>
+    <div className="  my-1  m-0  w-[100%]   mx-1   flex  justify-center items-center">
+       
+<img className=" w-[100vw]  md:h-[38vh]   sm:w-[100vw] sm:h-[50vh]    " src="/javarm.png" alt="My Image" />
+
+
+</div>
+<div className=' flex justify-center items-center '>
+    <h2 className=' md:text-[55px] text-centerc text-orange-500 '>
+        $49 per pouch
+    </h2><br/>
+   
+</div>
+<h3 className=' md:text-2xl text-center text-green-600'>
+        you save $888 !
+    </h3>
+<div className=' mx-10  animate-bounce  rounded-md    bg-blue-500   flex justify-center items-center'>
+<button className='   border-l md:text-[33px]  text-white  text-center'><Link href={'https://a61434z8ukmedw94gfb5wb-x91.hop.clickbank.net'}>Add to cart</Link> </button>
+</div>
+</div>
+
+<div className=' rounded-md shadow-lg  border-2 border-red-400 overflow-hidden'>
+    <h1 className=' text-center  md:text-[44px]'>
+            3 purchase
+        </h1>
+        <br/>
+        <p className=' text-2xl text-center'>
+           90  day supply
+        </p>
+    <div className="  my-1  m-0  w-[100%]   mx-1   flex  justify-center items-center">
+       
+<img className=" w-[100vw]  md:h-[38vh]   sm:w-[100vw] sm:h-[50vh]    " src="/javarm.png" alt="My Image" />
+
+
+</div>
+<div className=' flex justify-center items-center '>
+    <h2 className=' md:text-[55px] text-centerc text-orange-500 '>
+        $69 per pouch
+    </h2>
+</div>
+<h3 className=' md:text-2xl text-center text-green-600'>
+        you save $384 !
+    </h3>
+<div className=' mx-10  animate-bounce  rounded-md    bg-blue-500   flex justify-center items-center'>
+<button className='   border-l md:text-[33px]  text-white  text-center'><Link href={'https://a61434z8ukmedw94gfb5wb-x91.hop.clickbank.net'}>Add to cart</Link> </button>
+</div>
+</div>
+</div>
+
+
+
+
+<div className=' mt-32 bg-black '>
+    <h1 className=' sm:mt-14 sm:text-[23px] md:mx-10 text-center text-white md:text-[66px]'>
+    100% MONEY BACK GUARANTEE
+    </h1>
+<div className='sm:mx-3 md:mx-28'>
+<p className=' text-white sm:text-[17px] md:text-2xl'>
+This product is backed by a 100% money back guarantee for 60 full days from your original purchase. If you're not totally and completely satisfied with this product, your results or your experience in the first 60 days from your purchase simply let us know by calling our toll free number or dropping us an email and we'll give you a full refund within 48 hours of the product being returned. That's right, simply return the product, even empty pouches, anytime within 60 days of your purchase and you'll receive a full, no questions asked refund (less shipping and handling).
+</p>
+</div>
+</div>
+<div className=' sm:mx-4'>
+    <p className=' md:text-2xl'>
+    Are you struggling to lose weight no matter how much you diet or exercise? Turns out, it's not your fault.
+
+In 2023, after studying over 170 years of scientific data, a team of scientific researchers discovered that up to 80% of Americans may have simply inherited a slow and inefficient metabolism to some degree.
+
+Yet, this is where science gets very interesting…
+
+There’s one thing many of us enjoy every morning that can naturally prime our metabolism for fat-burning throughout the day…
+
+And that one thing is COFFEE!
+
+Yep, turns out coffee doesn’t just help us wake-up, it can also help wake-up our metabolism as well!
+
+For decades scientists have been fascinated by the mysterious effects that coffee, and it's over 1000 bio-active compounds have on the body, including metabolism…
+
+And although most of us can relate to the natural boost of energy, focus, alertness and mental clarity our morning coffee delivers us daily…
+
+Latest research shows coffee can be the “Perfect Primer” for fat-burning because it naturally opens a window of opportunity to boost the speed and efficiency of metabolism.
+
+You just need to add the right super-nutrients to do so.
+    </p>
+</div>
+
     </div>
   )
 }
